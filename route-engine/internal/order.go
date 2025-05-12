2a7b10b4096adb5aaf322f8daefdb9bcf31d53c1
Muhammad Illham Hanafi <illhamhanafi@gmail.com>
2025-05-12 19:58:32 +0700

    add comment for next improvement order creation

--- route-engine/internal/order.go
+++ route-engine/internal/order.go
@@ -9,6 +9,7 @@ import (
 )
 
 func (s *Service) ProcessOrder(c *gin.Context, order model.Order) {
+	// to do: cache for faster process
 	res, err := s.repository.GetActiveRoutingLogic(c.Request.Context())
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
@@ -19,6 +20,7 @@ func (s *Service) ProcessOrder(c *gin.Context, order model.Order) {
 	}
 
 	allocatedCourier := getCourierFromProbability(res.AllocationLogic)
+	// to do: cache
 	cour, err := s.repository.GetCourierByCode(c.Request.Context(), allocatedCourier)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
@@ -27,6 +29,7 @@ func (s *Service) ProcessOrder(c *gin.Context, order model.Order) {
 		})
 	}
 
+	// to do: make proses async using message queue
 	err = s.repository.InsertRoutingDecisionLog(c.Request.Context(), repository.InsertRoutingDecisionLogParams{
 		OrderID:           order.ID,
 		CourierID:         cour.ID,
